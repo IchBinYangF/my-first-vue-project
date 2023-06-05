9df0a8a9b2998b89cdb62f6e975660d62f8383b2
Yang <yang.fu@di-on.solutions>
2023-06-05 18:54:58 +0200

    finish vue+amplify tutorial

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,47 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-import TuForm from './components/TuForm.vue'
-import TuParent from './components/TuParent.vue'
-import { ref, computed } from 'vue'
+import { ref, computed } from "vue";
+import { Authenticator } from '@aws-amplify/ui-vue';
+import '@aws-amplify/ui-vue/styles.css';
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
+import TuForm from "./components/TuForm.vue";
+import TuParent from "./components/TuParent.vue";
+import AmToDo from "./components/AmToDo.vue";
+
 
 const routes = {
-  '/':TheWelcome,
-  '/tuform': TuForm,
-  '/tuparent': TuParent
-}
+  "/": TheWelcome,
+  "/tuform": TuForm,
+  "/tuparent": TuParent,
+  "/amtodo": AmToDo,
+};
 
-const currentPath = ref(window.location.hash)
-window.addEventListener('hashchange', () => {
-  currentPath.value = window.location.hash
-})
+const currentPath = ref(window.location.hash);
+window.addEventListener("hashchange", () => {
+  currentPath.value = window.location.hash;
+});
 
 const currentView = computed(() => {
-  return routes[currentPath.value.slice(1) || '/'] || NotFound
-})
-
+  return routes[currentPath.value.slice(1) || "/"] || NotFound;
+});
 </script>
 
 <template>
+  <authenticator>
+    <template v-slot="{ user, signOut }">
+      <h1>Hello {{ user.username }}!</h1>
+      <a href="#/amtodo">AmToDo</a>
+      <button @click="signOut">Sign Out</button>
+    </template>
+  </authenticator>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="./assets/logo.svg"
+      width="125"
+      height="125"
+    />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
@@ -32,8 +49,7 @@ const currentView = computed(() => {
   </header>
 
   <main>
-    <a href="#/">TheWelcome</a> |
-    <a href="#/tuform">TuForm</a> |
+    <a href="#/">TheWelcome</a> | <a href="#/tuform">TuForm</a> |
     <a href="#/tuparent">TuParent</a>
     <component :is="currentView" />
   </main>
