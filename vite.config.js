f789ab8f473afd54e90690e87ff9201984d21bf1
Yang <yang.fu@di-on.solutions>
2023-06-05 16:59:17 +0200

    Add amplify

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
+      "./runtimeConfig": "./runtimeConfig.browser",
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
