7b917c9f6074544db817d3b7f6f065b32607e116
Yang <yang.fu@di-on.solutions>
2023-06-06 08:16:17 +0200

    Bugfix: yarn dev global undifine isseus

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import { Amplify } from 'aws-amplify';
 import awsExports from './aws-exports';
+
+(window).global = window;
 Amplify.configure(awsExports);
 createApp(App).mount('#app')
+
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,32 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  ...(process.env.NODE_ENV === 'development'
+    ? {
+      define: {
+        global: {},
+      },
+    }
+    : {}),
   plugins: [vue()],
   resolve: {
-    alias: {
-      "./runtimeConfig": "./runtimeConfig.browser",
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+    alias: [
+      {
+        find: "./runtimeConfig",
+        replacement: "./runtimeConfig.browser",
+      },
+      {
+        find: "@",
+        replacement: fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    ],
+    // alias: {
+    //   "./runtimeConfig": "./runtimeConfig.browser",
+    //   '@': fileURLToPath(new URL('./src', import.meta.url))
+    // }
+  },
+});
